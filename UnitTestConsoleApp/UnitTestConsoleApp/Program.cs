d0ad692632b76562af66669b8f2148eafa41c620
Andrew Barker <androidboy@gmail.com>
2013-04-11 18:44:55 -0400

    inital project creation

---
+++ UnitTestConsoleApp/UnitTestConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "hello this is a change";
+        }
+    }
+}
